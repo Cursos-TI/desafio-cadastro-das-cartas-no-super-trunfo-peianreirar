39594c417cd6193f15d947a2c1a9470bb3b0a86d
peianreirar <124331908+peianreirar@users.noreply.github.com>
2025-04-09 10:26:17 +0000

    declarei as variáveis e solicitei entrada de dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,16 +7,68 @@
 //Teste larissa
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    //Declaração de variáveis carta 1
+
+    char estado, codigo[4], nome[50];
+    int populacao, PontosTuristicos;
+    float area, PIB;
+
+    //Declaração de variáveis carta 2
+
+    char estado2, codigo2[4], nome2[50];
+    int populacao2, Pontosturisticos2;
+    float area2, PIB2;
+
+    //Entrada de dados da carta 1 
+
+    printf("Digite uma letra que represente o estado da carta 1 (de A a H)");
+    scanf(" %c", &estado);
+
+    printf("Digite o código da carta 1 (letra que represente o estado + um número de 1 a 4)");
+    scanf("%s", codigo);
+
+    printf("Digite o nome da cidade");
+    scanf("%s", nome);
+
+    printf("Digite a população da cidade");
+    scanf("%d", &populacao);
+
+    printf("Digite a quantidades de pontos turísticos da cidade");
+    scanf("%d", &PontosTuristicos);
+
+    printf("Digite a área da cidade (em km²)");
+    scanf("%f", &area);
+
+    printf("Digite o PIB (Produto Interno Bruto) da cidade");
+    scanf("%f", &PIB);
+
+    //Entrada de dados carta 2
+
+    printf("Digite uma letra que represente o estado da carta 2 (de A a H)");
+    scanf(" %c", &estado2);
+
+    printf("Digite o código da carta 2 (letra que represente o estado + um número de 1 a 4)");
+    scanf("%s", codigo2);
+
+    printf("Digite o nome da cidade")
+    scanf("%s", nome2);
+
+    printf("Digite a população da cidade");
+    scanf("%d", &populacao2);
+
+    printf("Digite a quantidade de pontos turisticos da cidade");
+    scanf("%d", Pontosturisticos2);
+
+    printf("Digite a área da cidade (em km²)");
+    scanf("%f", &area2);
+
+    printf("Digite o PIB (Produto Interno Bruto) da cidade");
+    scanf("%f", &PIB2);
+
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+
+
+
 
     return 0;
 }
